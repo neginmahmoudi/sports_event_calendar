c85aba24dc9b73df42eebf546122ca790837762b
Negin Mahmoudi <negin.mahmoudii76@gmail.com>
2022-12-19 20:39:06 +0100

    Add bootstrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    require('bootstrap/dist/js/bootstrap.bundle.min.js');
+  }, []);
+  return <Component {...pageProps} />;
 }
